f7137ece7e062d007d470f4c93a36c70fff9c9c9
Tuomas Venhola <tuomas.venhola@cgi.com>
2017-03-17 17:34:12 +0200

    Modify script to reset target dir after pushing, remove hsl references

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e
-ORG=hsldevcom
 DOCKER_IMAGE=opentripplanner-data-container
 
 # Set these environment variables
-#DOCKER_TAG=
-#DOCKER_EMAIL=
-#DOCKER_USER=
-#DOCKER_AUTH=
+ORG=
+DOCKER_TAG=latest
+DOCKER_EMAIL=
+DOCKER_USER=
+DOCKER_AUTH=
 
 # Build image
 docker build --tag="$ORG/$DOCKER_IMAGE:builder" -f Dockerfile.builder .
 mkdir target
-docker run --rm --entrypoint tar hsldevcom/opentripplanner-data-container:builder -c /opt/opentripplanner-data-container/webroot|tar x -C target 
+docker run --rm --entrypoint tar $ORG/opentripplanner-data-container:builder -c /opt/opentripplanner-data-container/webroot|tar x -C target 
 docker build --tag="$ORG/$DOCKER_IMAGE:$DOCKER_TAG" -f Dockerfile .
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_AUTH
 docker push $ORG/$DOCKER_IMAGE:$DOCKER_TAG
-docker tag -f $ORG/$DOCKER_IMAGE:$DOCKER_TAG $ORG/$DOCKER_IMAGE:latest
+docker tag $ORG/$DOCKER_IMAGE:$DOCKER_TAG $ORG/$DOCKER_IMAGE:latest
 docker push $ORG/$DOCKER_IMAGE:latest
+
+rm -rf target
